b2e1ce7f4b902a2b6f158f536efaf1a406fc9ba3
skykanin <3789764+skykanin@users.noreply.github.com>
2020-03-01 02:14:44 +0100

    Remove comments and desktop.nix

--- nix/machines/desktop.nix
+++ nix/machines/desktop.nix
@@ -16,14 +16,25 @@
     ];
 
   # Define hostname
-  networking.hostName = "daisy";
+  networking.hostName = "emma";
   # Set your time zone.
   time.timeZone = "Europe/Oslo";
 
-  programs.light.enable = true;
-  services.compton.vSync = true;
-  # Suspend on lid close
-  services.logind.lidSwitch = "suspend"; 
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
+    screenSection =
+      ''
+        Option         "metamodes" "DP-0: 1920x1080_144 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, HDMI-0: nvidia-auto-select +3840+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+      '';
+    xrandrHeads = [
+      { output = "DP-0"; primary = true; }
+      { monitorConfig =
+          ''
+            Option "Rotate" "Left"
+          '';        
+          output = "HDMI-0"; }
+    ];
+  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database

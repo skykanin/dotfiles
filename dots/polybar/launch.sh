029e763108c120ba909c12805665fd52f9f1d2bb
skykanin <3789764+skykanin@users.noreply.github.com>
2020-03-03 04:38:38 +0100

    Update fish i3 and polybar

--- dots/polybar/launch.sh
+++ dots/polybar/launch.sh
@@ -3,14 +3,24 @@
 # Terminate already running bar instances
 pkill polybar
 
-export DEFAULT_NETWORK_INTERFACE=$(ls /sys/class/net | grep wlp | head -n 1 | awk '{print $1}')
+network_interface=$(ls /sys/class/net | grep wlp | head -n 1 | awk '{print $1}')
+export TRAY_POSITION="left"
+export DEFAULT_NETWORK_INTERFACE="$network_interface"
+
+# Monkey patch until real fix
+# xrandr --output HDMI-0 --rotate left --right-of DP-2
+
+# Wait for i3 to fully start..
+# i3 isn't fully started when it execs this file, and polybar fails to connect
+# to the i3 socket (or gets denied) and never shows up
+sleep 0.2
+
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+
+outputs=$(xrandr --query | grep " connected" | cut -d " " -f 1)
 
 for m in $outputs; do
-  export MONITOR=$m
-  export TRAY_POSITION=right
-  polybar --reload primary &
+  MONITOR=$m polybar --reload primary &> ~/polybar.error &
   disown
 done

abe4d5a63d35d929661763ff1da9895896c58a77
skykanin <3789764+skykanin@users.noreply.github.com>
2020-03-05 04:32:57 +0100

    Add spotify module

---
+++ scripts/spotify.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Show Spotify current song
+
+set -euo pipefail
+
+spotifyon=$(pgrep spotify)
+if [ -z "$spotifyon" ]; then
+    echo ''
+else
+artist=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify \
+	/org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get \
+	string:'org.mpris.MediaPlayer2.Player' string:'Metadata' \
+	| grep -E -A 2 "artist" | grep -E -v "artist" \
+	| grep -E -v "array" | cut -b 27-|cut -d '"' -f 1 | grep -E -v ^$)
+
+title=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'| grep -E -A 1 "title"| grep -E -v "title"|cut -b 44-|cut -d '"' -f 1| grep -E -v ^$)
+    echo "$artist" "-" "$title"
+fi

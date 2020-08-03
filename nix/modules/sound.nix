f06b68a63af2e6fd0a1d8d77f3cc14edc0b0946b
skykanin <3789764+skykanin@users.noreply.github.com>
2020-08-04 00:35:14 +0200

    Add 32bit support for pulseaudio

--- nix/modules/sound.nix
+++ nix/modules/sound.nix
@@ -5,6 +5,7 @@
   sound.enable = true;
   hardware.pulseaudio = {
     enable = true;
+    support32Bit = true;
     # Only the full build has Bluetooth support, so it must be selected here.
     package = pkgs.pulseaudioFull;
   };

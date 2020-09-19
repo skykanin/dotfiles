b581ffac7e5af94dd59d9075a6aab6081b55b742
skykanin <3789764+skykanin@users.noreply.github.com>
2020-09-19 13:41:52 +0200

    Add explicit ntfs support

--- nix/modules/boot-efi.nix
+++ nix/modules/boot-efi.nix
@@ -5,4 +5,5 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.supportedFilesystems = [ "ntfs" ];
 }

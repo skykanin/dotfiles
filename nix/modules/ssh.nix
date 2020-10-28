494a12217e18e29f919d8e629eb58277e1fc2363
skykanin <3789764+skykanin@users.noreply.github.com>
2020-10-28 03:37:00 +0100

    Fix sftp config

--- nix/modules/ssh.nix
+++ nix/modules/ssh.nix
@@ -2,10 +2,23 @@
 {
  # Enable the OpenSSH daemon.
   services.openssh = {
+    challengeResponseAuthentication = false;
+    passwordAuthentication = false;
     authorizedKeysFiles = [
       "$HOME/.ssh/id_rsa"
       "$HOME/.ssh/id_rsa_github"
       "$HOME/.ssh/hetzner_rsa"];
     enable = true;
+    allowSFTP = false;
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
+    extraConfig = ''
+      Subsystem sftp internal-sftp
+      XAuthLocation none
+    '';
   };
 }

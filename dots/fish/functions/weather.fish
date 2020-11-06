85c9da1f1ca9f998a61f10dfa765084f5fe07c9d
skykanin <3789764+skykanin@users.noreply.github.com>
2020-11-07 00:35:52 +0100

    Add location argument to weather function

--- dots/fish/functions/weather.fish
+++ dots/fish/functions/weather.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function weather --wraps=curl\ \'wttr.in/\?M\?F\' --description alias\ weather=curl\ \'wttr.in/\?M\?F\'
-  curl 'wttr.in/?M?F' $argv;
+  curl "wttr.in/$argv[1]?M?F" $argv[2..-1];
 end

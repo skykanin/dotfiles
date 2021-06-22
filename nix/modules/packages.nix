3fd86c91990072dd248b99efdc2e67ec48c5f4cc
skykanin <3789764+skykanin@users.noreply.github.com>
2021-06-22 18:55:34 +0200

    Remove idris2 from package list and format file

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -1,10 +1,8 @@
 { config, pkgs, ... }:
 
 with pkgs;
-let
-  stable = import <stable> { config.allowUnfree = true; };
-  in
-{
+let stable = import <stable> { config.allowUnfree = true; };
+in {
   environment.systemPackages = with pkgs; [
     adwaita-qt
     android-studio
@@ -44,7 +42,6 @@ let
     git
     gnome3.nautilus
     gnome3.networkmanagerapplet
-    idris2-git
     jetbrains.idea-ultimate
     joker
     kitty
@@ -100,33 +97,30 @@ let
     (self: super: {
       weechat-custom = super.weechat.override {
         configure = { availablePlugins, ... }: {
-        scripts = with super.weechatScripts; [
-          weechat-matrix
-        ];
+          scripts = with super.weechatScripts; [ weechat-matrix ];
+        };
       };
-    };
-   })
+    })
     (self: super: {
-      my-leiningen = super.leiningen.override {
-        jdk = pkgs.openjdk11;
-      };
+      my-leiningen = super.leiningen.override { jdk = pkgs.openjdk11; };
     })
     (self: super: {
       idris2-git = idris2.overrideAttrs (oldAttrs: {
         name = "idris2";
         version = null;
-        
+
         src = pkgs.fetchFromGitHub {
           owner = "idris-lang";
           repo = "Idris2";
-          rev = "6b071137624d72c0688315f2adecb3507a371027";
-          sha256 = "105ibnp5djp07cwnbzmr9vv4m2dhhcs99k62njmwfpjhpnxzj2zm";
+          rev = "82cf4092b725cfac19b341ee6d1d8ff80bd84d61";
+          sha256 = "171nm3vi9h5idlzr8s42vqyw2c8l034icg6hzj7bh0qbkxrpqxig";
         };
         buildFlags = [ "bootstrap" "SCHEME=scheme" ];
       });
     })
     (import (builtins.fetchTarball {
-      url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+      url =
+        "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
       sha256 = "0db7gp0mzzc0hzyrghfjycapdd9byybcga9flqgaywssmr17if69";
     }))
   ];

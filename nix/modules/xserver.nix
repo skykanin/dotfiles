b96fccc832857d77087bf2fe2bdacac06329f35b
skykanin <3789764+skykanin@users.noreply.github.com>
2021-06-30 20:12:13 +0200

    Switch from betterlockscreen to i3lock-color

--- nix/modules/xserver.nix
+++ nix/modules/xserver.nix
@@ -1,12 +1,25 @@
 { config, pkgs, ... }:
 {
   services = {
-    compton = {
+    picom = {
       backend = "glx";
       enable = true;
       refreshRate = 0; # automatically detect monitor refresh rate
       activeOpacity = 1.0;
-      inactiveOpacity = 1.0;      
+      inactiveOpacity = 1.0;
+      menuOpacity = 0.9;      
+      experimentalBackends = true;
+      settings = {
+        # blur-background = true;
+        # blur-background-frame = true;
+        # blur-background-fixed = true;
+        blur = {
+          method = "gaussian";
+          size = 30;
+          deviation = 5.0;
+        };
+        mark-overdir-focused = true;
+      };
     };
     
     xserver = {
@@ -45,10 +58,9 @@
         configFile = /home/skykanin/.config/i3/config;
         package = pkgs.i3-gaps;
         extraPackages = with pkgs; [
-          i3lock
+          i3lock-color
           rofi
           polybarFull
-          betterlockscreen
         ];
       };
 

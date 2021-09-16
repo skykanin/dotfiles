bdb365d58756876ed02112bf14418a5d846d804b
skykanin <3789764+skykanin@users.noreply.github.com>
2021-09-16 23:33:23 +0200

    Add nix-community binary cache

--- nix/modules/general.nix
+++ nix/modules/general.nix
@@ -40,8 +40,11 @@
   
   nix = {
     autoOptimiseStore = true;
-    binaryCaches = [ "https://hydra.iohk.io" ];
-    binaryCachePublicKeys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    binaryCaches = [ "https://hydra.iohk.io" "https://nix-community.cachix.org" ];
+    binaryCachePublicKeys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
     extraOptions = ''
        experimental-features = nix-command flakes ca-references
        keep-outputs = false 

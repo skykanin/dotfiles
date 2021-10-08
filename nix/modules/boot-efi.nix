4d97d0d3a0e9ddd1fe5b546c93d5198611cce3eb
skykanin <3789764+skykanin@users.noreply.github.com>
2021-10-08 23:57:10 +0200

    Set configuration limit for systemd-boot

--- nix/modules/boot-efi.nix
+++ nix/modules/boot-efi.nix
@@ -2,8 +2,13 @@
 
 {
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = { 
+    efi.canTouchEfiVariables = true;
+    systemd-boot = {
+      configurationLimit = 2; # limit boot entries
+      enable = true;
+    };
+  };
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.supportedFilesystems = [ "ntfs" ];
 }

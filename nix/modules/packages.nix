f472c6c549a9fd8915261f9d4993e4bcb0e6ab33
skykanin <3789764+skykanin@users.noreply.github.com>
2022-01-09 12:45:58 +0100

    Fix and add some packages

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -102,14 +102,14 @@ in {
     racket
     ripgrep
     rlwrap
-    rofi
+    rofiWithPlugins
     shellcheck
     slack
     spotifywm
     steam
     texlive.combined.scheme-full
     tldr
-    #tree
+    tree
     unzip
     vim-with-conf
     #vscode
@@ -146,11 +146,23 @@ in {
     #   }); 
     # })
     (self: super: {
+      rofiWithPlugins = let
+        rofi-hoogle-src = pkgs.fetchFromGitHub {
+          owner = "rebeccaskinner";
+          repo = "rofi-hoogle";
+          rev = "27c273ff67add68578052a13f560a08c12fa5767";
+          sha256 = "09vx9bc8s53c575haalcqkdwy44ys1j8v9k2aaly7lndr19spp8f";
+        };
+        rofi-hoogle = import "${rofi-hoogle-src}/release.nix";
+      in super.rofi.override { plugins = [ rofi-hoogle.rofi-hoogle ]; };
+    })
+    (self: super: {
       weechat-custom = super.weechat.override {
         configure = { availablePlugins, ... }: {
-          scripts = with super.weechatScripts; [
-            #weechat-matrix
-          ];
+          scripts = with super.weechatScripts;
+            [
+              #weechat-matrix
+            ];
         };
       };
     })

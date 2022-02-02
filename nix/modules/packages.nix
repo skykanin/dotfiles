78066a5a5d9bc2d8dd081d512332704198277e4c
skykanin <3789764+skykanin@users.noreply.github.com>
2022-02-02 23:06:26 +0100

    Add patch to fix i3lock-color from freezing

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -29,7 +29,7 @@ let
 in {
   environment.systemPackages = with pkgs; [
     adwaita-qt
-    android-studio
+    #android-studio
     #android-udev-rules
     arc-icon-theme
     arc-theme
@@ -196,6 +196,18 @@ in {
       });
     })
     (import emacs-overlay)
+    (final: prev:
+      let
+        patchedPkgs = import (builtins.fetchTarball {
+          url =
+            "https://github.com/nixos/nixpkgs/archive/ffdadd3ef9167657657d60daf3fe0f1b3176402d.tar.gz";
+          sha256 = "1nrz4vzjsf3n8wlnxskgcgcvpwaymrlff690f5njm4nl0rv22hkh";
+        }) {
+          inherit (prev) system config;
+          # inherit (prev) overlays;  # not sure
+        };
+        patchedPam = patchedPkgs.pam;
+      in { i3lock-color = prev.i3lock-color.override { pam = patchedPam; }; })
     # xmonad.overlay
     # xmonad-contrib.overlay
   ];

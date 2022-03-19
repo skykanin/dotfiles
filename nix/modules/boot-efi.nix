7ed60b2c04233d6d706c7afc87bb967ecdecf3e2
skykanin <3789764+skykanin@users.noreply.github.com>
2022-03-19 14:01:47 +0100

    Refactor boot efi module

--- nix/modules/boot-efi.nix
+++ nix/modules/boot-efi.nix
@@ -2,18 +2,20 @@
 
 {
   # Use the systemd-boot EFI boot loader.
-  boot.loader = { 
-    efi.canTouchEfiVariables = true;
-    systemd-boot = {
-      configurationLimit = 5; # limit boot entries
-      enable = true;
+  boot = {
+    loader = { 
+      efi.canTouchEfiVariables = true;
+      systemd-boot = {
+        configurationLimit = 5; # limit boot entries
+        enable = true;
+      };
     };
+    kernelPackages = pkgs.linuxPackages_latest;
+    supportedFilesystems = [ "btrfs" "ntfs" ];
+    extraModprobeConfig = ''
+      options kvm_intel nested=1
+      options kvm_intel emulate_invalid_guest_state=0
+      options kvm ignore_msrs=1
+    '';
   };
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.supportedFilesystems = [ "btrfs" "ntfs" ];
-  boot.extraModprobeConfig = ''
-    options kvm_intel nested=1
-    options kvm_intel emulate_invalid_guest_state=0
-    options kvm ignore_msrs=1
-  '';
 }

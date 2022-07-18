0e14f65f3741a343672dea4110c1aa7afdc22039
skykanin <3789764+skykanin@users.noreply.github.com>
2022-07-18 15:42:29 +0200

    cleanup commented out code

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -195,13 +195,8 @@ in {
           url =
             "https://github.com/nixos/nixpkgs/archive/ffdadd3ef9167657657d60daf3fe0f1b3176402d.tar.gz";
           sha256 = "1nrz4vzjsf3n8wlnxskgcgcvpwaymrlff690f5njm4nl0rv22hkh";
-        }) {
-          inherit (prev) system config;
-          # inherit (prev) overlays;  # not sure
-        };
+        }) { inherit (prev) system config; };
         patchedPam = patchedPkgs.pam;
       in { i3lock-color = prev.i3lock-color.override { pam = patchedPam; }; })
-    # xmonad.overlay
-    # xmonad-contrib.overlay
   ];
 }

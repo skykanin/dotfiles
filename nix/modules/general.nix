891c8490c35f097eb1e24bc43749d6ed2a8ef46a
skykanin <3789764+skykanin@users.noreply.github.com>
2022-11-03 16:32:39 +0100

    Remove scrive cache

--- nix/modules/general.nix
+++ nix/modules/general.nix
@@ -46,14 +46,12 @@
         "https://hydra.iohk.io"
         "https://iohk.cachix.org"
         "https://nix-community.cachix.org"
-        "https://scrive.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "scrive.cachix.org-1:U0qIgICaW+EuvCoqaYbbHR8JKTGNi29w4d+7Bc4LWfU="
       ];
       trusted-substituters = [ "https://hydra.iohk.io" ];
       trusted-users = [ "root" "skykanin" ];

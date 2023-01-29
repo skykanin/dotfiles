c0b42bca926bdc967d080671c830c576ff6954a3
skykanin <3789764+skykanin@users.noreply.github.com>
2023-01-29 13:53:54 +0100

    switch to dracula theme

--- dots/emacs/doom/config.el
+++ dots/emacs/doom/config.el
@@ -166,7 +166,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-dracula)
 
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")

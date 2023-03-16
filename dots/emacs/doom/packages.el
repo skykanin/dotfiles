f885d4c971c02bf94513ef021344ef9a23b89baf
skykanin <3789764+skykanin@users.noreply.github.com>
2023-03-16 11:53:51 +0100

    pin magit deps

--- dots/emacs/doom/packages.el
+++ dots/emacs/doom/packages.el
@@ -17,6 +17,15 @@
 
 (package! unisonlang-mode)
 
+;; Pin magit deps
+(package! transient
+      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+      :recipe (:host github :repo "magit/transient"))
+
+(package! with-editor
+          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+          :recipe (:host github :repo "magit/with-editor"))
+
 ;; NOTE: It's possible to disable parts of lsp-ui as well see docs
 ;; https://emacs-lsp.github.io/lsp-mode/tutorials/how-to-turn-off/
 ;; (package! lsp-ui :disable t)

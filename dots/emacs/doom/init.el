00c79643a3acd165904ec8aef4569072933d15fa
skykanin <3789764+skykanin@users.noreply.github.com>
2023-03-16 11:54:52 +0100

    rearrange modules

--- dots/emacs/doom/init.el
+++ dots/emacs/doom/init.el
@@ -127,6 +127,7 @@
        emacs-lisp          ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
        ;;ess               ; emacs speaks statistics
+       ;;factor
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
@@ -142,8 +143,7 @@
        ;;julia             ; a better, faster MATLAB
        kotlin              ; a better, slicker Java(Script)
        latex               ; writing papers in Emacs has never been so fun
-       ;;lean
-       ;;factor
+       ;;lean              ; for folks with too much to prove
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
        markdown            ; writing docs for people to ignore

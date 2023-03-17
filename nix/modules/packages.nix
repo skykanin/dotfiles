7b3979b2763fb34192f285160fd3e12adde8bb97
skykanin <3789764+skykanin@users.noreply.github.com>
2023-03-17 21:11:09 +0100

    add cbqn overlay for newer version

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -43,7 +43,7 @@ in {
     bottom
     brightnessctl
     cachix
-    (cbqn.override { enableReplxx = true; }) 
+    cbqn-replxx
     chatterino2
     clj-kondo
     clojure
@@ -142,6 +142,18 @@ in {
     #     postPatch = '''';
     #   }); 
     # })
+    # Update CBQN to newer commit so that we get the latest features
+    (final: prev: {
+      cbqn = prev.cbqn.overrideAttrs (oldAttrs: {
+        version = "0.pre+date=2023-02-01";
+        src = pkgs.fetchFromGitHub {
+          owner = "dzaima";
+          repo = "CBQN";
+          rev = "05c1270344908e98c9f2d06b3671c3646f8634c3";
+          sha256 = "wKeyYWMgTZPr+Ienz3xnsXeD67vwdK4sXbQlW+GpQho=";
+        };
+      });
+    })
     (import emacs-overlay)
     (final: prev:
       let

e7606ac62355f136d3bde7825131bb59ba46764b
skykanin <3789764+skykanin@users.noreply.github.com>
2023-04-16 19:33:15 +0200

    update bqn-mode package version

--- dots/emacs/doom/packages.el
+++ dots/emacs/doom/packages.el
@@ -48,7 +48,7 @@
 (package! bqn-mode
   :recipe (:host github
            :repo "museoa/bqn-mode")
-  :pin "8dd46215c4534a057758bd4ef15173d2820cefa7")
+  :pin "b1f7f062b87dc5de84e0f00b7b5d895bf862af90")
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format

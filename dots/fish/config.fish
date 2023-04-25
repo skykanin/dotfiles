5e70eca6942dfd43eae5f84554964148339f09f5
skykanin <3789764+skykanin@users.noreply.github.com>
2023-04-25 18:47:21 +0200

    add pnpm stuff to fish config

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -2,3 +2,10 @@ alias cat=bat
 eval (direnv hook fish)
 
 source ~/.config/fish/nnn.fish
+
+# pnpm
+set -gx PNPM_HOME "/home/skykanin/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end

74bd347d2d80c30a6721a3f1c43b859bfe352058
skykanin <3789764+skykanin@users.noreply.github.com>
2023-05-15 13:00:13 +0200

    more git aliases :)

--- dots/fish/functions/grs.fish
+++ dots/fish/functions/grs.fish
@@ -1,4 +1,4 @@
-# Defined in - @ line 1
 function grs --wraps='git restore' --description 'alias grs=git restore'
-  git restore $argv;
+  git restore $argv
+        
 end
---
+++ dots/fish/functions/grst.fish
@@ -0,0 +1,4 @@
+function grst --wraps='git restore --staged' --description 'alias grst=git restore --staged'
+  git restore --staged $argv
+        
+end

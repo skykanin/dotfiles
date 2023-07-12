870622cd464597676300a8fd54765551e363df11
skykanin <3789764+skykanin@users.noreply.github.com>
2023-07-12 19:48:57 +0200

    Unload 'suspend-on-idle' pulseaudio module
    Not sure if this does anything, but it's good to
    do anyways.

--- nix/modules/sound.nix
+++ nix/modules/sound.nix
@@ -8,5 +8,7 @@
     support32Bit = true;
     # Only the full build has Bluetooth support, so it must be selected here.
     package = pkgs.pulseaudioFull;
+    # Disable power saving
+    extraConfig = "unload-module module-suspend-on-idle";
   };
 }

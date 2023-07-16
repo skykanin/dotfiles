9070131776f52f3b052a0f657ea2a1bc7f4eba5d
skykanin <3789764+skykanin@users.noreply.github.com>
2023-07-16 15:10:37 +0200

    switch lockscreen command

--- nix/modules/xserver/xautolock.nix
+++ nix/modules/xserver/xautolock.nix
@@ -2,7 +2,7 @@
 
 let
   lock-script = pkgs.writeShellScript "lock-script" ''
-     ${pkgs.i3lock}/bin/i3lock -c 000000 -e -f
+     ${pkgs.i3lock}/bin/i3lock -c 000000 -e -f -i ~/Pictures/wallpapers/evening-forest2k.png
   '';
 in {
   inherit enable time;

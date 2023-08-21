9c3f8f77cbf153c86c72fcb6772c73424033475b
skykanin <3789764+skykanin@users.noreply.github.com>
2023-08-21 17:11:56 +0200

    remove generated host key

--- nix/modules/ssh.nix
+++ nix/modules/ssh.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }: {
-  # Enable the OpenSSH daemon.
+  # OpenSSH daemon.
   services.openssh = {
     enable = true;
     settings = {
@@ -8,10 +8,6 @@
       PermitRootLogin = "without-password";
     };
     allowSFTP = false;
-    hostKeys = [{
-      path = "/etc/ssh/ssh_host_ed25519_key";
-      type = "ed25519";
-    }];
     extraConfig = ''
       Subsystem sftp internal-sftp
       XAuthLocation none

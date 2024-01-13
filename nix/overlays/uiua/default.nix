cca9515344e0e2926e1c0bf563687721ba6f5a53
skykanin <3789764+skykanin@users.noreply.github.com>
2024-01-13 23:51:36 +0100

    update uiua package

--- nix/overlays/uiua/default.nix
+++ nix/overlays/uiua/default.nix
@@ -1,18 +1,18 @@
 final: prev: {
   uiua = prev.uiua.overrideAttrs (finalAttrs: prevAttrs: {
     name = "uiua-${finalAttrs.version}";
-    version = "0.6.1";
+    version = "0.7.1";
 
     src = prev.fetchFromGitHub {
       owner = "uiua-lang";
       repo = "uiua";
       rev = finalAttrs.version;
-      hash = "sha256-/yeLsuwEKw6+jBKd7CAnR9RuVaKwXjVpcvO3v0FaAck=";
+      hash = "sha256-cBwQdArVRiXH8TmgBSPpcB5oNu3Q/+Us9Azzw0lV5Vs=";
     };
 
     cargoDeps = prevAttrs.cargoDeps.overrideAttrs (_: {
       inherit (finalAttrs) src;
-      outputHash = "sha256-4tR1n96s91EFZLO4RIBpNKLjOSbGrBIApJrS60RBuQQ=";
+      outputHash = "sha256-7cgKiEqklvUw64a6+lbHA9t6QWiTquYVi0evXkONEag=";
     });
   });
 }

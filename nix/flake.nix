441d35819a2de02ce708be04737541dacb396d84
Nicholas Jaunsen <3789764+skykanin@users.noreply.github.com>
2024-02-23 14:46:03 +0100

    Update nixd setup in flake.nix
    This also prepares the flake to compatable on macOS for my
    nix-darwin setup that's coming soon:tm:

--- nix/flake.nix
+++ nix/flake.nix
@@ -21,7 +21,7 @@
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
+      systems = ["aarch64-darwin" "x86_64-linux"];
 
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
@@ -89,7 +89,7 @@
       }: {
         devShells.default = pkgs.mkShell {
           name = "nixd-shell";
-          buildInputs = with pkgs; [nixd];
+          packages = with pkgs; [nixd];
         };
 
         formatter = pkgs.alejandra;

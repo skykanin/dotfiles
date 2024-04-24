d1a295dadb5c907f5938cc60c32d41facb8ad0ee
Nicholas Jaunsen <3789764+skykanin@users.noreply.github.com>
2024-04-24 15:13:46 +0200

    simplify emacs config

--- nix/modules/emacs.nix
+++ nix/modules/emacs.nix
@@ -7,7 +7,6 @@
   services.emacs = {
     enable = true;
     package =
-      (pkgs.emacsPackagesFor (pkgs.emacs29.override {withPgtk = true;}))
-      .emacsWithPackages (epkgs: [epkgs.vterm]);
+      pkgs.emacs29-pgtk.pkgs.emacsWithPackages (epkgs: [epkgs.vterm]);
   };
 }

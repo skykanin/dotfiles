54301b9da86d6665688cb2fa1e4b0bd06e5a5597
Nicholas Jaunsen <3789764+skykanin@users.noreply.github.com>
2024-07-02 14:15:17 +0200

    Add fix for fish shell $PATH bug with nix-darwin

--- nix/modules/programs/default.nix
+++ nix/modules/programs/default.nix
@@ -16,8 +16,20 @@
         config.enable = true;
         functions.enable = true;
       };
+      # See fish issue with nix-darwin: https://github.com/LnL7/nix-darwin/issues/122#issuecomment-1659465635
+      loginShellInit =
+        if pkgs.stdenv.isDarwin
+        then let
+          # This naive quoting is good enough in this case. There shouldn't be any
+          # double quotes in the input string, and it needs to be double quoted in case
+          # it contains a space (which is unlikely!)
+          dquote = str: "\"" + str + "\"";
+          makeBinPathList = map (path: path + "/bin");
+        in ''
+          fish_add_path --move --prepend --path ${lib.concatMapStringsSep " " dquote (makeBinPathList config.environment.profiles)}
+          set fish_user_paths $fish_user_paths
+        ''
+        else "";
     };
-
-
   };
 }

c72fa5d55f582bec29ac47375c9ac5740ce3f0c7
skykanin <3789764+skykanin@users.noreply.github.com>
2024-09-03 08:59:04 +0200

    fix formatting

--- nix/packages/httpie-desktop.nix
+++ nix/packages/httpie-desktop.nix
@@ -11,9 +11,9 @@
     pname = "httpie";
     version = "2024.1.2";
     src = fetchurl {
-        url = "https://github.com/httpie/desktop/releases/download/v${version}/HTTPie-${version}.AppImage";
-        hash = "sha256-OOP1l7J2BgO3nOPSipxfwfN/lOUsl80UzYMBosyBHrM=";
-      };
+      url = "https://github.com/httpie/desktop/releases/download/v${version}/HTTPie-${version}.AppImage";
+      hash = "sha256-OOP1l7J2BgO3nOPSipxfwfN/lOUsl80UzYMBosyBHrM=";
+    };
     appimage = appimageTools.wrapType2 {inherit pname version src;};
     appimage-contents = appimageTools.extractType2 {inherit pname version src;};
   in

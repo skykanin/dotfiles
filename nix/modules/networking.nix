fc15a223ab26c2b1ff6a0db2db905cab1f71b75a
skykanin <3789764+skykanin@users.noreply.github.com>
2024-11-09 14:56:36 +0100

    close ports in firewall config desktop

--- nix/modules/networking.nix
+++ nix/modules/networking.nix
@@ -14,8 +14,8 @@ in {
   config.networking = {
     firewall = lib.mkIf cfg.firewall.enable {
       enable = true;
-      allowedTCPPorts = [53 80 443 3333 4568 8080 8096 8920 9630 44330 19000 19001 19002 19003];
-      allowedUDPPorts = [53 80 443 9630 44330 19000 19001 19002 19003];
+      allowedTCPPorts = [3333 4568 8080 8096 8920 9630 44330 19000 19001 19002 19003];
+      allowedUDPPorts = [9630 44330 19000 19001 19002 19003];
     };
     networkmanager = {
       enable = cfg.networkmanager.enable;

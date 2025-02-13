b1632b91e0d6ca4ae61743890c4f6c84262796bf
skykanin <3789764+skykanin@users.noreply.github.com>
2025-02-13 14:57:01 +0100

    add nnn plugins deps to PATH

---
+++ nix/overlays/nnn/default.nix
@@ -0,0 +1,19 @@
+final: prev: {
+  # Adds packages to PATH that are needed by the preview-tui NNN plugin
+  # to work with additional file formats
+  nnn = prev.nnn.overrideAttrs(oldAttrs: {
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ])
+        ++ [ final.makeWrapper ];
+    postInstall = ''
+      ${oldAttrs.postInstall or ""}
+
+      wrapProgram $out/bin/nnn \
+        --prefix PATH : "${final.lib.makeBinPath (with final; [
+          bat
+          ffmpegthumbnailer
+          gnome-epub-thumbnailer
+          poppler_utils
+        ])}"
+    '';
+  });
+}

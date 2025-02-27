a0fc55a8bb043221f96218e0906ede4479e261da
skykanin <3789764+skykanin@users.noreply.github.com>
2025-02-27 18:56:02 +0100

    fix nix formatting

--- nix/overlays/nnn/default.nix
+++ nix/overlays/nnn/default.nix
@@ -1,18 +1,21 @@
 final: prev: {
   # Adds packages to PATH that are needed by the preview-tui NNN plugin
   # to work with additional file formats
-  nnn = prev.nnn.overrideAttrs(oldAttrs: {
-    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ])
-        ++ [ final.makeWrapper ];
+  nnn = prev.nnn.overrideAttrs (oldAttrs: {
+    nativeBuildInputs =
+      (oldAttrs.nativeBuildInputs or [])
+      ++ [final.makeWrapper];
     postInstall = ''
       ${oldAttrs.postInstall or ""}
 
       wrapProgram $out/bin/nnn \
-        --prefix PATH : "${final.lib.makeBinPath (with final; [
+        --prefix PATH : "${final.lib.makeBinPath (with final;
+        [
           bat
           ffmpegthumbnailer
           poppler_utils
-        ] ++ final.lib.optional final.stdenv.isLinux final.gnome-epub-thumbnailer)}"
+        ]
+        ++ final.lib.optional final.stdenv.isLinux final.gnome-epub-thumbnailer)}"
     '';
   });
 }

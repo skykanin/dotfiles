7a0d747807af393a0f51fbf7f41176bf8e79d454
skykanin <3789764+skykanin@users.noreply.github.com>
2025-03-02 19:47:34 +0100

    add regreet using gamescope to greetd module

--- nix/modules/desktop/wayland/greetd.nix
+++ nix/modules/desktop/wayland/greetd.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.local.desktop.greetd;
@@ -9,17 +10,37 @@ in {
     lib.mkEnableOption "Enable greetd login service";
 
   config = lib.mkIf cfg.enable {
+    programs.regreet = {
+      enable = true;
+
+      # cageArgs = [ "-s" "-m" "last" ];
+
+      theme = {
+        package = pkgs.tokyonight-gtk-theme;
+        name = "Tokyonight-Dark";
+      };
+
+      iconTheme = {
+        package = pkgs.numix-icon-theme;
+        name = "Numix Circle";
+      };
+
+      settings = {
+        background = {
+          path = "/home/skykanin/Pictures/wallpapers/planets.jpg";
+          fit = "Contain";
+        };
+
+        env.ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+      };
+    };
+
     services.greetd = {
       enable = true;
       settings = {
-        default_session = {
-          command = let
-            wm =
-              if config.local.desktop.hyprland.enable
-              then config.programs.hyprland.package
-              else config.programs.sway.package;
-          in "${lib.getExe wm} 2> ~/sway.log";
-          user = config.local.user.name;
+        default_session = let cfg-rgt = config.programs.regreet; in {
+          command = "${lib.getExe pkgs.gamescope} -H 1440 -O DP-1 -f --backend drm -- ${lib.getExe cfg-rgt.package}";
+          user = "greeter";
         };
       };
     };

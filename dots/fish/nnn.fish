4c7479dd2cece13ed122410c6f68751d70baaba4
skykanin <3789764+skykanin@users.noreply.github.com>
2025-03-24 17:47:00 +0100

    add NNN keybinding for copying filepath

--- dots/fish/nnn.fish
+++ dots/fish/nnn.fish
@@ -1,5 +1,5 @@
 # NNN config
-export NNN_PLUG='p:preview-tui;t:preview-tabbed;i:imgview;v:vidthumb;u:~/dotfiles/scripts/nnn_unzip.sh'
+export NNN_PLUG='p:preview-tui;t:preview-tabbed;i:imgview;v:vidthumb;u:~/dotfiles/scripts/nnn_unzip.sh;c:! echo $PWD/$nnn|wl-copy*'
 alias nnn "nnn -e"
 export NNN_FIFO="/tmp/nnn.fifo"
 export NNN_TRASH=1

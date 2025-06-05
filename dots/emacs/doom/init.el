391899274285e48a9269cdf764d931fd8a836bd0
skykanin <3789764+skykanin@users.noreply.github.com>
2025-06-05 19:33:06 +0200

    enable tree-sitter for python and scala

--- dots/emacs/doom/init.el
+++ dots/emacs/doom/init.el
@@ -163,7 +163,8 @@
        purescript          ; javascript, but functional
        (python             ; beautiful is better than ugly
         +pyenv
-        +lsp)
+        +lsp
+        +tree-sitter)
        ;;qt                ; the 'cutest' gui framework ever
        racket              ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -171,7 +172,9 @@
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust                ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       (scala +lsp)        ; java, but good
+       (scala              ; java, but good
+        +lsp
+        +tree-sitter)
        scheme              ; a fully conniving family of lisps
        sh                  ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml

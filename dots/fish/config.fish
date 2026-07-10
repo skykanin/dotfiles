cab6dfa77ec10627d4b178cdbf84f1afe1c1a05b
skykanin <3789764+skykanin@users.noreply.github.com>
2026-07-10 12:13:04 +0200

    fish: make ENVVAR OS agnostic

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -3,7 +3,7 @@ alias cat=bat
 source ~/.config/fish/nnn.fish
 
 # pnpm
-set -gx PNPM_HOME "/home/skykanin/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end

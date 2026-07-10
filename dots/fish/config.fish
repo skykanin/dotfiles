5730d5147b4d601bfecaa6495b3af51a49f400b8
skykanin <3789764+skykanin@users.noreply.github.com>
2026-07-10 12:42:21 +0200

    add pnpm bin to path

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -4,8 +4,8 @@ source ~/.config/fish/nnn.fish
 
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
 end
 # pnpm end
 

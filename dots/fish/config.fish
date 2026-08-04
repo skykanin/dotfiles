566e27fa7531e31d94ef5f2de94c30fdd93add49
skykanin <3789764+skykanin@users.noreply.github.com>
2026-08-04 08:37:26 +0200

    fish: make envvars platform agnostic

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -2,6 +2,40 @@ alias cat=bat
 
 source ~/.config/fish/nnn.fish
 
+set -l user_path_candidates \
+  $HOME/.local/bin \
+  $HOME/.npm-global/bin \
+  $HOME/.npm-global \
+  $HOME/.config/emacs/bin \
+  $HOME/.cabal/bin \
+  $HOME/.emacs.d/bin
+
+set -l existing_user_paths
+for path in $user_path_candidates
+  if test -d $path
+    set -a existing_user_paths $path
+  end
+end
+
+if test (count $existing_user_paths) -gt 0
+  fish_add_path --global $existing_user_paths
+end
+
+switch (uname)
+  case Darwin
+    if test -d /opt/homebrew/bin
+      fish_add_path --global /opt/homebrew/bin
+    end
+
+    if command -q /usr/libexec/java_home
+      set -gx JAVA_HOME (/usr/libexec/java_home 2>/dev/null)
+    end
+  case Linux
+    if test -d /run/current-system/sw/bin
+      fish_add_path --global /run/current-system/sw/bin
+    end
+end
+
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- "$PNPM_HOME/bin" $PATH

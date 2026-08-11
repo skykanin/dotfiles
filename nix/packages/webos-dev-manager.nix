f29263fb58b61201883a3e9fcb0596f25425fc31
skykanin <3789764+skykanin@users.noreply.github.com>
2026-08-11 22:49:03 +0200

    webdev-os-manager: 1.99.16 -> 1.99.19

--- nix/packages/webos-dev-manager.nix
+++ nix/packages/webos-dev-manager.nix
@@ -5,14 +5,14 @@
     fetchurl,
     lib,
   }:
-    let version = "1.99.16";
+    let version = "1.99.19";
     in appimageTools.wrapType2 {
       pname = "webos-dev-manager";
       inherit version;
 
       src = fetchurl {
         url = "https://github.com/webosbrew/dev-manager-desktop/releases/download/v${version}/webos-dev-manager_${version}_amd64.AppImage";
-        hash = "sha256-1Eg8flL81vJXcGG9492tePqI4LpvEap2spuYtfIwAKU=";
+        hash = "sha256-MMUxWGqvTdmyQCY7UuvWiGxdHx3vzfeLgVF6NGnmD5c=";
       };
 
       meta = with lib; {
